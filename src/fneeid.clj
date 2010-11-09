2170f7aab3501a9ac31b1186ff2e454be67b22c3
Pepijn de Vos <pepijndevos@gmail.com>
2010-11-09 21:06:26 +0100

    working example

--- src/fneeid.clj
+++ src/fneeid.clj
@@ -1,9 +1,27 @@
 (ns fneeid)
 
-(defn identify [question function positive negative]
-  (fn [input truth]
-    (let [resfn (if truth positive negative)
-          result (remove #(= truth (not (function %))) input)]
-      (if (fn? resfn)
-        (partial resfn result)
-        result))))
+(defn identify 
+  "It's complicated"
+  [question function positive negative]
+  (with-meta
+    (fn [input truth]
+      (let [resfn (if truth positive negative)
+            result (filter (if truth function (complement function)) input)]
+        (if (fn? resfn)
+          (with-meta (partial resfn result) (meta resfn))
+          result)))
+    {:question question}))
+
+(let [q (identify "Are you working on a scalar?"
+                  #(some #{'coll} (flatten (:arglists (meta (resolve %)))))
+                  nil
+                  nil)]
+  (def graph (with-meta (partial q (keys (ns-publics 'clojure.core))) (meta q))))
+
+(loop [q graph]
+  (if (fn? q)
+    (do
+      (print (:question (meta q)) "[yn] ")
+      (flush)
+      (recur (q (#{"y" "yes"} (read-line)))))
+    (println q)))
